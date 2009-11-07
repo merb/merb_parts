4c1c17895e069f2fc079215cef053d93dbd6f12e
Pavel Kunc <pavel.kunc@gmail.com>
2009-11-07 01:06:35 +0000

    Major maintenance and dumping new version.
    Adding Merb::Parts::VERSION, CHANGELOG, and dumping 0.9.14 version.
    Also switching to Jeweler for the gem management.

---
+++ lib/merb_parts/version.rb
@@ -0,0 +1,5 @@
+module Merb
+  module Parts 
+    VERSION    = '0.9.14'.freeze
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,22 @@
 require 'rubygems'
+
+# Use current merb-core sources if running from a typical dev checkout.
+lib = File.expand_path('../../../merb/merb-core/lib', __FILE__)
+$LOAD_PATH.unshift(lib) if File.directory?(lib)
 require "merb-core"
-require File.join( File.dirname(__FILE__), "..", "lib", "merb_parts" )
+
+# The lib under test
+require 'merb_parts'
 
 # Require the fixtures
 Dir[File.join(File.dirname(__FILE__), "fixtures", "*/**.rb")].each{|f| require f }
 
-Merb.start :environment => 'test',
-           :adapter => 'runner',
-           :log_level => :error,
-           :disabled_components => [:json]
+Merb.start(
+  :environment => 'test',
+  :adapter => 'runner',
+  :log_level => :error,
+  :disabled_components => [:json]
+)
 
 Spec::Runner.configure do |config|
   config.include Merb::Test::RequestHelper  
